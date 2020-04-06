b368859c53451d8e8cd89acfef6c26382c2675ed
Matt <5638426+mattbruv@users.noreply.github.com>
2020-04-05 22:44:06 -0400

    add really hacky controls, TEMPORARY

--- client/public/main.ts
+++ client/public/main.ts
@@ -2,12 +2,64 @@ import { loadSpriteSheet } from "../src/render/textures";
 import { GameClient } from "../src/client";
 import Cookies from "js-cookie";
 import { Localizer } from "../src/localization/localizer";
+import {
+  planeData,
+  PlaneType,
+  planeConstants
+} from "../../dogfight/src/objects/plane";
 
 let client: GameClient;
 
 function init(): void {
   // create game client engine
   client = new GameClient();
+}
+
+function createInput(title: string, plane: string, key: string): void {
+  const desc = document.createElement("span") as HTMLSpanElement;
+  desc.innerHTML = title;
+  const input = document.createElement("input") as HTMLInputElement;
+  input.type = "text";
+  input.value = planeData[plane][key];
+  input.onchange = (e): void => {
+    const newValue = parseInt(e.target.value);
+    planeData[plane][key] = newValue;
+    console.log(planeData);
+  };
+  document.body.appendChild(desc);
+  document.body.appendChild(input);
+  document.body.appendChild(document.createElement("br"));
+}
+
+function inputConstants(title: string): void {
+  const desc = document.createElement("span") as HTMLSpanElement;
+  desc.innerHTML = title;
+  const input = document.createElement("input") as HTMLInputElement;
+  input.type = "text";
+  input.value = planeConstants[title];
+  input.onchange = (e): void => {
+    const newValue = parseInt(e.target.value);
+    planeConstants[title] = newValue;
+    console.log(planeConstants);
+  };
+  document.body.appendChild(desc);
+  document.body.appendChild(input);
+  document.body.appendChild(document.createElement("br"));
+}
+
+function addControls(): void {
+  console.log("ADDING CONTROLS");
+  inputConstants("GRAVITY");
+  inputConstants("THRUST");
+
+  // add plane info adjustors
+  const vals = ["speed", "turnRate", "flightTime"];
+  for (const v of vals) {
+    for (const plane in planeData) {
+      const strPlane = PlaneType[plane];
+      createInput(strPlane + " " + v, plane, v);
+    }
+  }
 }
 
 /**
@@ -39,5 +91,6 @@ function addLanguageSelect(): void {
 
 window.addEventListener("load", (): void => {
   loadSpriteSheet(init);
+  addControls();
   addLanguageSelect();
 });
